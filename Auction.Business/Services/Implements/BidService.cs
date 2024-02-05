2f1bf653e0c4e844d3cbb7548e6ab4bcb7b0b17a
rockeywerewolf <mi71k9d7p@code.edu.az>
2024-02-05 21:10:05 +0400

    bid crud

---
+++ Auction.Business/Services/Implements/BidService.cs
@@ -0,0 +1,101 @@
+﻿using Auction.Business.DTOs.BidDTOs;
+using Auction.Business.DTOs.ItemDTOs;
+using Auction.Business.Exceptions.Bid;
+using Auction.Business.Exceptions.Common;
+using Auction.Business.Repositories.Interfaces;
+using Auction.Business.Services.Interfaces;
+using Auction.Core.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.Business.Services.Implements;
+
+public class BidService : IBidService
+{
+    readonly IBidRepository _repo;
+    readonly IItemRepository _itemRepo;
+    readonly IMapper _mapper;
+    readonly IHttpContextAccessor _contextAccessor;
+    readonly string _userId;
+
+    public BidService(IBidRepository repo, IMapper mapper, IHttpContextAccessor contextAccessor, IItemRepository itemRepo)
+    {
+        _repo = repo;
+        _mapper = mapper;
+        _contextAccessor = contextAccessor;
+        if (_contextAccessor.HttpContext.User.Claims.Any())
+        {
+            _userId = _contextAccessor.HttpContext?.User?.Claims?.First(x => x.Type == ClaimTypes.NameIdentifier)?.Value ?? throw new NullReferenceException();
+        }
+        _itemRepo = itemRepo;
+    }
+
+    public async Task CreateAsync(BidCreateDTO dto)
+    {
+        var data = _mapper.Map<Bid>(dto);
+        if (!await _itemRepo.IsExistAsync(r => r.Id == dto.ItemId))
+            throw new NotFoundException<Item>();
+        var item = await _itemRepo.GetByIdAsync(dto.ItemId);
+        if (!item.Bids.Any())
+            if (dto.Amount < item.CurrentPrice)
+                throw new InvalidBidAmountException();
+        if (dto.Amount <= item.CurrentPrice)
+            throw new InvalidBidAmountException();
+        item.CurrentPrice = dto.Amount;
+        await _itemRepo.SaveAsync();
+        data.BidderId = _userId;
+        await _repo.CreateAsync(data);
+        await _repo.SaveAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var data = await _checkId(id);
+        _repo.Delete(data);
+        await _repo.SaveAsync();
+    }
+
+    public IEnumerable<BidListDTO> GetAll()
+        => _mapper.Map<IEnumerable<BidListDTO>>(_repo.GetAll(includes: new[] { "Bidder", "Item" }));
+
+    public async Task<BidDetailsDTO> GetByIdAsync(int id)
+    {
+        var data = await _checkId(id, true);
+        return _mapper.Map<BidDetailsDTO>(data);
+    }
+
+    public async Task UpdateAsync(int id, BidUpdateDTO dto)
+    {
+        var data = await _checkId(id);
+        data = _mapper.Map(dto, data);
+        await _repo.SaveAsync();
+    }
+
+    public async Task SoftDelete(int id)
+    {
+        var data = await _checkId(id);
+        data.IsDeleted = true;
+        await _repo.SaveAsync();
+    }
+
+    public async Task ReverseSoftDelete(int id)
+    {
+        var data = await _checkId(id);
+        data.IsDeleted = false;
+        await _repo.SaveAsync();
+    }
+
+    async Task<Bid> _checkId(int id, bool isTrack = false)
+    {
+        if (id <= 0) throw new ArgumentException();
+        var data = await _repo.GetByIdAsync(id, isTrack, "Bidder", "Item");
+        if (data == null) throw new NotFoundException<Bid>();
+        return data;
+    }
+}
---
+++ Auction.Business/Services/Interfaces/IBidService.cs
@@ -0,0 +1,20 @@
+﻿using Auction.Business.DTOs.BidDTOs;
+using Auction.Business.DTOs.CategoryDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.Business.Services.Interfaces;
+
+public interface IBidService
+{
+    public IEnumerable<BidListDTO> GetAll();
+    public Task<BidDetailsDTO> GetByIdAsync(int id);
+    public Task CreateAsync(BidCreateDTO topic);
+    public Task DeleteAsync(int id);
+    public Task UpdateAsync(int id, BidUpdateDTO topic);
+    public Task SoftDelete(int id);
+    public Task ReverseSoftDelete(int id);
+}
